2333b33ea03dd8379065bd9dce5e52cf3bf9614a
mathiex <mathias1381@gmx.com>
2025-05-24 08:13:16 +0200

    Several bugs fixed

---
+++ config.lua
@@ -0,0 +1,8 @@
+
+extraTiles = {
+    [1] = {xOffset = -2, yOffset = -1, txd = "minimap_sea_Galapagous_1", txn = "minimap_sea_Galapagous_1"}, -- Galapagous Mid
+    [2] = {xOffset = -3, yOffset = -1, txd = "minimap_sea_Galapagous_2", txn = "minimap_sea_Galapagous_2"}, -- Galapagous Left
+    [3] = {xOffset = -2, yOffset = 0, txd = "minimap_sea_Galapagous_3", txn = "minimap_sea_Galapagous_3"}, -- Galapagous Top
+}
+
+defaultAlpha = 100
